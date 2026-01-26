72e0b2242f3627743bfdda46f1d99dc1d55b8137
antonio-decaro <antonio.decaro99@outlook.it>
2026-01-26 16:24:43 +0100

    Initialize head in SharedQueue and implement pop method for element retrieval

--- library/include/utils/queue.cuh
+++ library/include/utils/queue.cuh
@@ -13,16 +13,31 @@ namespace clutra::detail::utils {
 template<size_t Capacity>
 struct SharedQueue {
   int tail;
+  int head;
   uint32_t vertices[Capacity];
   uint32_t degrees[Capacity];
 
-  __device__ void init() { tail = 0; }
+  __device__ void init() { tail = 0; head = 0;}
 
   __forceinline__ __device__ int push(uint32_t vertex, uint32_t degree) {
   const int loc = atomicAdd(&tail, 1);
     vertices[loc] = vertex;
     degrees[loc] = degree;
     return loc;
+  }
+
+  __forceinline__ __device__ bool pop(uint32_t& vertex, uint32_t& degree) {
+    if (head < 0 || head >= tail) {
+      return false; // empty
+    }
+    
+    vertex = vertices[head];
+    degree = degrees[head];
+    
+    if (threadIdx.x == 0) {
+      atomicAdd(&head, 1);
+    }
+    return true;
   }
 
   __device__ int size() const { return tail; }
