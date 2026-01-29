f21d080c9bf469f3340ca913ffbcae975efaf5ee
antonio-decaro <antonio.decaro99@outlook.it>
2026-01-29 11:57:51 +0100

    refactor: Simplify logging function by removing debug log preprocessor directives and using environment variable check

--- library/include/utils/logging.cuh
+++ library/include/utils/logging.cuh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdlib>
 #include <fmt/core.h>
 #include <iostream>
 #include <string_view>
@@ -9,13 +10,12 @@ namespace clutra::detail {
 
 template<typename... Args>
 inline void log(std::string_view message, Args&&... args) {
-#if defined(CLUTRA_DEBUG_LOG) && CLUTRA_DEBUG_LOG
+  if (std::getenv("CLUTRA_LOGGING") == nullptr) {
+    return;
+  }
+
   std::cerr << "\x1b[35m"
             << fmt::format(fmt::runtime(message), std::forward<Args>(args)...)
             << "\x1b[0m" << std::endl;
-#else
-  (void)message;
-  (void)sizeof...(args);
-#endif
 }
 } // namespace clutra::detail
